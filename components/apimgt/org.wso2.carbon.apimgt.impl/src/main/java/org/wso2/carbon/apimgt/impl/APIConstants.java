f870c8a14c329471c9983bbd797ef9d50cfb0bd8
tharikaGitHub <tharika.m92@gmail.com>
2019-07-08 15:49:21 +0530

    Add Rest API for Ratings

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
@@ -1612,4 +1612,9 @@ public final class APIConstants {
         API,
         PRODUCT_API
     }
+    //Constants for user API ratings
+    public static final String API_ID = "apiId";
+    public static final String RATING_ID = "ratingId";
+    public static final String RATING = "rating";
+    public static final String USER_NAME ="username";
 }
