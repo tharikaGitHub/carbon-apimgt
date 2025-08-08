ebe08974453c6198aadac8c253760c38283437da
Tharika Madurapperuma <tharika@wso2.com>
2025-08-08 06:45:32 +0530

    Add new columns to store gateway mode and scheduled interval

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -15467,6 +15467,8 @@ public class ApiMgtDAO {
                     String description = rs.getString("DESCRIPTION");
                     String provider = rs.getString("PROVIDER");
                     String gatewayType = rs.getString("GATEWAY_TYPE");
+                    String mode = rs.getString("MODE");
+                    int scheduledTime = rs.getInt("SCHEDULED_TIME");
                     Map<String, String> additionalProperties = new HashMap();
                     try (InputStream configuration = rs.getBinaryStream("CONFIGURATION")) {
                         if (configuration != null) {
@@ -15486,6 +15488,8 @@ public class ApiMgtDAO {
                     env.setDescription(description);
                     env.setProvider(provider);
                     env.setGatewayType(gatewayType);
+                    env.setMode(mode);
+                    env.setApiDiscoveryScheduledWindow(scheduledTime);
                     env.setVhosts(getVhostGatewayEnvironments(connection, id));
                     env.setPermissions(getGatewayVisibilityPermissions(uuid));
                     env.setAdditionalProperties(additionalProperties);
@@ -15521,6 +15525,8 @@ public class ApiMgtDAO {
                     String description = rs.getString("DESCRIPTION");
                     String provider = rs.getString("PROVIDER");
                     String gatewayType = rs.getString("GATEWAY_TYPE");
+                    String mode = rs.getString("MODE");
+                    int scheduledTime = rs.getInt("SCHEDULED_TIME");
                     Map<String, String> additionalProperties = new HashMap();
                     try (InputStream configuration = rs.getBinaryStream("CONFIGURATION")) {
                         if (configuration != null) {
@@ -15539,6 +15545,8 @@ public class ApiMgtDAO {
                     env.setDescription(description);
                     env.setProvider(provider);
                     env.setGatewayType(gatewayType);
+                    env.setMode(mode);
+                    env.setApiDiscoveryScheduledWindow(scheduledTime);
                     env.setVhosts(getVhostGatewayEnvironments(connection, id));
                     env.setPermissions(getGatewayVisibilityPermissions(uuid));
                     env.setAdditionalProperties(additionalProperties);
@@ -15578,6 +15586,8 @@ public class ApiMgtDAO {
                 String configurationJson = new Gson().toJson(environment.getAdditionalProperties());
                 prepStmt.setBinaryStream(8, new ByteArrayInputStream(configurationJson.getBytes()));
                 prepStmt.setString(9, tenantDomain);
+                prepStmt.setString(10, environment.getMode());
+                prepStmt.setInt(11, environment.getApiDiscoveryScheduledWindow());
                 prepStmt.executeUpdate();
 
                 GatewayVisibilityPermissionConfigurationDTO permissionDTO = environment.getPermissions();
@@ -15773,7 +15783,8 @@ public class ApiMgtDAO {
                 prepStmt.setString(2, environment.getDescription());
                 String configurationJson = new Gson().toJson(environment.getAdditionalProperties());
                 prepStmt.setBinaryStream(3, new ByteArrayInputStream(configurationJson.getBytes()));
-                prepStmt.setString(4, environment.getUuid());
+                prepStmt.setInt(4, environment.getApiDiscoveryScheduledWindow());
+                prepStmt.setString(5, environment.getUuid());
                 prepStmt.executeUpdate();
                 deleteGatewayVhosts(connection, environment.getId());
                 addGatewayVhosts(connection, environment.getId(), environment.getVhosts());
